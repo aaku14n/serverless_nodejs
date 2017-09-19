a3b43c6d223aeb4d7159fa98f96005016b9a0a5b
aaku14n <akarshgdps@gmail.com>
2017-09-19 16:37:43 +0530

    fetch and delete

--- fetch.js
+++ fetch.js
@@ -1,8 +1,27 @@
-const uuid = require("uuid");
-// import AWS from "aws-sdk";
 const MongoClient = require("mongodb").MongoClient;
+const bluebird = require("bluebird");
 const assert = require("assert");
-module.exports.Read = (event, context, callback) => {
-  const id_2 = event.pathPatameters.id;
-  console.log(id_2);
+
+module.exports.main = (event, context, callback) => {
+  const id = "3d368560-9d2a-11e7-aed7-a5109e79613d";
+  if (id) {
+    MongoClient.connect(process.env.atlas_connection_uri, (err, db) => {
+      assert.equal(null, err);
+      try {
+        db.collection("Cluster0").findOne({ user_id: id }, (error, result) => {
+          if (error) {
+            callback(null, { statusCode: 500, body: JSON.stringify(error) });
+          } else if (result !== null) {
+            callback(null, { statusCode: 200, result });
+            db.close();
+          } else {
+            callback(null, { statusCode: 200, body: "User dose not exist" });
+          }
+        });
+        db.close();
+      } catch (err) {
+        callback(err);
+      }
+    });
+  }
 };
